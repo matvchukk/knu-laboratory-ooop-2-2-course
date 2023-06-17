9060dfaef6ee5f360dbc9b3c855a257880639fb0
Anastasiia Matviichuk <a.matviychuk0831@gmail.com>
2023-06-17 18:21:07 +0300

    Added matrix algorithms

---
+++ lab 3/matrix_algorithms/GaussJordan.h
@@ -0,0 +1,84 @@
+#include "ComplexMatrix.h"
+
+ComplexMatrix GaussJordanInverse(ComplexMatrix A)
+{
+    int rank = A.get_rows(); //rows
+    int columns = A.get_columns(); //columns
+
+    assert(rank == columns);
+    assert(rank == A.getRank());
+
+    ComplexMatrix tempMatrix(rank, 2 * rank);
+
+    //setting the temp matrix
+    for (int i = 0; i < rank; i++)
+    {
+        for (int j = 0; j < 2 * rank; j++)
+        {
+            if (j < rank)
+                tempMatrix.set(i, j, A.get(i, j));
+            if (j == (i + rank))
+                tempMatrix.set(i, j, 1, 0);
+        }
+    }
+
+    for (int i = 0; i < rank; i++)
+    {
+        tempMatrix.set(i, i + rank, 1, 0);
+    }
+
+    //    //interchanging the rows
+    //    for (int i = rank - 1; i > 0; i--)
+    //    {
+    //        if (tempMatrix.get(i-1,0).get_real() < tempMatrix.get(i,0).get_real())
+    //        {
+    //            tempMatrix.swapRows(i, i-1);
+    //        }
+    //    }
+
+    //    std::cout << std::endl;
+    //    tempMatrix.print();
+
+    for (int i = 0; i < rank; i++)
+    {
+
+        bool swapped = false;
+        while (tempMatrix.get(i, i) == ComplexNum(0, 0)) { tempMatrix.swapRows(i, i + 1); swapped = true; }
+        if (swapped) { i = std::max(0, i - 1); continue; }
+        if (tempMatrix.get(i, i) == ComplexNum(0, 0)) throw std::exception(); // checking nulls on a principal diagonal
+
+        for (int j = 0; j < rank; j++)
+        {
+            if (i != j)
+            {
+                ComplexNum temp = tempMatrix.get(j, i) / tempMatrix.get(i, i);
+
+                for (int k = 0; k < 2 * rank; k++)
+                {
+                    tempMatrix.set(j, k, tempMatrix.get(j, k) - tempMatrix.get(i, k) * temp);
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < rank; i++)
+    {
+        ComplexNum temp = tempMatrix.get(i, i);
+        for (int j = 0; j < 2 * rank; j++)
+        {
+            tempMatrix.set(i, j, tempMatrix.get(i, j) / temp);
+        }
+    }
+
+    ComplexMatrix resMatrix(rank, rank);
+
+    for (int i = 0; i < rank; i++)
+    {
+        for (int j = rank; j < 2 * rank; j++)
+        {
+            resMatrix.set(i, j - rank, tempMatrix.get(i, j));
+        }
+    }
+
+    return resMatrix;
+}
---
+++ lab 3/matrix_algorithms/matrix_algorithms.cpp
@@ -0,0 +1,75 @@
+﻿// matrix_algorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "ComplexNum.h"
+#include "ComplexMatrix.h"
+#include "LU_Inverse.h"
+#include "GaussJordan.h"
+
+int main() {
+    // Set the seed for random number generation
+    std::srand(std::time(0));
+
+    // Generate random matrix
+    int matrixSize = 4;
+    ComplexMatrix A(matrixSize, matrixSize);
+    A.auto_gen(5, 50, 5, 50);
+
+    std::cout << "Random Matrix A:" << std::endl;
+    A.print();
+
+    // Perform LU decomposition
+    ComplexMatrix L(matrixSize, matrixSize);
+    ComplexMatrix U(matrixSize, matrixSize);
+    if (LU_decomposition(A, L, U)) {
+        std::cout << "LU Decomposition:" << std::endl;
+        std::cout << "L:" << std::endl;
+        L.print();
+        std::cout << "U:" << std::endl;
+        U.print();
+    }
+    else {
+        std::cout << "LU decomposition failed." << std::endl;
+    }
+
+    // Perform inverse matrix calculation using LU decomposition
+    ComplexMatrix inverseA = LU_inverse(A);
+    if (inverseA.get_rows() != 0) {
+        std::cout << "Inverse Matrix:" << std::endl;
+        inverseA.print();
+    }
+    else {
+        std::cout << "Inverse matrix calculation failed." << std::endl;
+    }
+
+    // Generate random matrix for Gauss-Jordan inversion
+    int matrixRank = 10;
+    ComplexMatrix randomMatrix(matrixRank, matrixRank);
+    randomMatrix.auto_gen(1, 10, 1, 10);
+
+    std::cout << "Random Matrix for Gauss-Jordan Inversion:" << std::endl;
+    randomMatrix.print();
+
+    // Perform Gauss-Jordan inversion
+    ComplexMatrix inverseMatrix = GaussJordanInverse(randomMatrix);
+    std::cout << "Inverse Matrix using Gauss-Jordan Inversion:" << std::endl;
+    inverseMatrix.print();
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
